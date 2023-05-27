a26de8df1e7b72ad832aee25173be5a8e292ff7e
Sergey Moiseev <skifride@gmail.comm>
2023-05-27 16:38:56 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+void FindeThirdGigit(int number)
+{
+    if(number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+   
+    if(number < 1000)
+    if(number > 99)
+    {
+        int result = number / 10;
+        Console.WriteLine("Третья цифра: " + result % 10);
+
+    
+    }
+
+    if(number > 9999)
+    if(number < 100000)
+    {
+        int result = number / 100;
+        Console.WriteLine("Третья цифра: " + result % 10);
+    }
+}
+
+Console.WriteLine("введите число:");
+int num = int.Parse(Console.ReadLine());
+FindeThirdGigit(num);

f20c2b8799de9101963b2f4380adfc519afa4aed
Sergey Moiseev <skifride@gmail.comm>
2023-05-27 12:52:45 +0300

    add task10

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+void FindAverageDigit(int number)
+{
+    int first_secondDigit = number  / 10;
+    Console.WriteLine("Вторая цифра числа: " + first_secondDigit % 10);
+} 
+
+int num = new Random().Next(99,1000);
+Console.WriteLine("Рандомное число: "+num);
+FindAverageDigit(num);
+
+int num1 = new Random().Next(300,700);
+Console.WriteLine("Рандомное число: "+num1);
+FindAverageDigit(num1);
+
+int num2 = new Random().Next(900,1000);
+Console.WriteLine("Рандомное число: "+num2);
+FindAverageDigit(num2);

a0f6863bfb5c1bdab2099e5435469641848d8e6f
Sergey Moiseev <skifride@gmail.comm>
2023-05-31 18:32:55 +0300

    add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number = Prompt("Введите число: ");
+PrintDegreTable(number);
+
+int Prompt(String message)
+{
+Console.Write(message);
+return int.Parse(Console.ReadLine()); ;
+}
+
+void PrintDegreTable(int number)
+{
+for (int i = 2; i <= number; i++)
+{
+Console.Write(Math.Pow(i, 3) + " ");
+}
+}

171d1f4ea4ace2f1f53ea11258439a438e7c69b8
Sergey Moiseev <skifride@gmail.comm>
2023-06-02 22:52:57 +0300

    add task29

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+Console.WriteLine("Ведите 8 чисел:");
+
+
+int [] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("");
+    array[i] = int.Parse(Console.ReadLine());   
+}
+
+
+void PrintArray(int[]array1)
+{
+Console.Write("[");
+for (int i = 0; i <array1.Length -1; i++)
+{
+    Console.Write($"{array1[i]}, ");
+}
+Console.Write($"{array1[array1.Length -1]}");
+Console.Write("]");
+}
+
+PrintArray(array);

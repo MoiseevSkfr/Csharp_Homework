e6669569db949e69a15a77e33b1c8bee58ce2979
Sergey Moiseev <skifride@gmail.comm>
2023-06-15 15:22:00 +0300

    add Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+int InPutNum(string message) // ввод данных
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void ChekTrueTriangel (int num, int num2, int num3) 
+{
+if (num<num2+num3 && num2<num+num3 && num3<num+num2) {
+Console.WriteLine($"Такой треугольник существует");}
+else Console.WriteLine($"Такой треугольник не существует");
+}
+
+
+int a = InPutNum("введите первую сторону");
+int b = InPutNum("введите первую сторону");
+int c = InPutNum("введите первую сторону");
+ChekTrueTriangel (a,b,c);
---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int InPutNum(string message) // ввод данных
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int a = InPutNum("Введите число");
+int res = 0;
+string showresult = string.Empty;
+while (a > 0)
+{
+    res = a % 2;
+    a /= 2;
+    showresult = res + showresult;
+}
+Console.WriteLine();
+Console.WriteLine(showresult);
